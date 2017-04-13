9af41a6f646eb595cb8cd47e7f68f4db48fabf6c
lambhunter0 <george.theophanous@hotmail.com>
2017-04-13 19:12:02 -0400

    fixes

--- DigitRecognition/src/drivers/Main.java
+++ DigitRecognition/src/drivers/Main.java
@@ -15,19 +15,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		try {
-<<<<<<< HEAD
-			ArgumentParser ap = new ArgumentParser();			
-			ap.addArgument("--naive-loader", new NaiveFeatureLoader());
-			ap.addArgument("--clustering", new ClusteringAlgorithm());
-=======
 			ArgumentParser ap = new ArgumentParser();
 			
-			ap.addArgument("--naive-loader", new NaiveFeatureLoader("Features\\naive\\feature", 20, 20));
+			ap.addArgument("--naive-loader", new NaiveFeatureLoader("Features/naive/feature", 20, 20));
 			ap.addArgument("--skeleton-loader", new NaiveFeatureLoader("Features\\skeleton\\feature", 15, 15));
 			ap.addArgument("--clustering-paper", new ClusteringAlgorithm(new PaperSimilarity(2.0f, 2.5f)));
 			ap.addArgument("--clustering-rt", new ClusteringAlgorithm(new RogersTanimoto()));
 			ap.addArgument("--clustering-jaccard", new ClusteringAlgorithm(new JaccardSimilarity()));
->>>>>>> origin/master
+
 			ap.addArgument("--decision-tree", new DecisionTree());
 			ap.addArgument("--naive-bayes", new NaiveBayes());						
 			ap.addArgument("--mlp", new MultiLayerPerceptron(10,3)); //# hidden neurons, # hidden layers
